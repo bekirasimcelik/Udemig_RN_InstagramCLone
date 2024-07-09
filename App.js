a08b32808a993c7ffa0f34b186e9ea2b1978bd69
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-07-09 12:49:51 +0300

    LogBox added

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Screen from './src/Screen';
 import {SafeAreaView} from 'react-native';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
